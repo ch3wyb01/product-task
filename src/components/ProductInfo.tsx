5ac268d797ae8bbb684d2ad72a71ef8abe450184
Rebecca Sullivan <beccasullivan30@gmail.com>
2025-12-07 18:44:34 +0000

    feat: set min height on product info

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from "@mui/material";
-import type { ProductPrice } from "../api/types";
+import { Box, Typography } from '@mui/material';
+import type { ProductPrice } from '../api/types';
 
 export interface ProductInfoProps {
     productName: string;
@@ -7,10 +7,37 @@ export interface ProductInfoProps {
 }
 export const ProductInfo = ({ productName, price }: ProductInfoProps) => {
     return (
-        <Box display='flex' flexDirection='column' justifyContent="space-between" alignItems="flex-start" gap={2} height='100%'>
-            <Typography variant="h6" fontSize='16px' sx={{ WebkitLineClamp: 2, overflow: 'hidden', textOverflow: 'ellipsis', WebkitBoxOrient: 'vertical', display: '-webkit-box', textAlign: 'left' }}>{productName}</Typography>
-            <Typography variant='body2' color='secondary.main' fontWeight='
-            bold'>£{price.priceIncTax} {price.currencyCode}</Typography>
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            alignItems="flex-start"
+            gap={2}
+            height="100%"
+        >
+            <Typography
+                variant="h6"
+                fontSize="16px"
+                sx={{
+                    WebkitLineClamp: 2,
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    WebkitBoxOrient: 'vertical',
+                    display: '-webkit-box',
+                    textAlign: 'left',
+                    minHeight: '3.2em',
+                }}
+            >
+                {productName}
+            </Typography>
+            <Typography
+                variant="body2"
+                color="secondary.main"
+                fontWeight="
+            bold"
+            >
+                £{price.priceIncTax} {price.currencyCode}
+            </Typography>
         </Box>
     );
-}
+};
